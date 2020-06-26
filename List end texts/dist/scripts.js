2b65a2e9f9c1efbe877437c4e0a224628609eb26
Netti89 <64971106+Netti89@users.noreply.github.com>
2020-06-26 19:12:52 +0200

    Initial commit

---
+++ List end texts/dist/scripts.js
@@ -0,0 +1,16 @@
+let names=['Greta','Lora','Benett','Netti'];
+
+names.forEach(function (name) {$('ul').append(name);})
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+  function myAdditionalFunction () 
+  {$('ul').append(`<h1>${additionalBlock.title}<h1>`);
+   $('ul').append(`<p>${additionalBlock.text}<p>`);}
+  
+  myAdditionalFunction()
+
+  
