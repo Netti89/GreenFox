3bdb9a389e8fd96007608e9c5fb403f4f0f7f5b4
Netti89 <64971106+Netti89@users.noreply.github.com>
2020-06-26 21:56:20 +0200

    completa

--- My First Little Page/dist/scripts.js
+++ My First Little Page/dist/scripts.js
@@ -1,3 +1,11 @@
 $('#FirstButton').click(function () {console.log("Yeah,you clicked me.");});
 $('#SecondButton').click(function () {$('#FirstButton').text('You clicked on the Second Button.')});
 $('#ThirdButton').click(() => {$('button').css('background','green');});
+
+$('#text').val('blue');
+var color;
+$('#last').click(function() {
+    color = $('#text').val();
+    {$('button').css('background',`${color}`);}
+});
+
